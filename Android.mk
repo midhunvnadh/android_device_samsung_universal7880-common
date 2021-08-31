31f6240378c07c4bdacbf759bfbc04e2437a86b3
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-08-31 17:40:36 +0000

    Changes
    Change-Id: I3788c1cda9867a1270ebf0ba3b883ac15bde85e0

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter m20lte m30lte a40 a30, $(TARGET_DEVICE)),)
+ifneq ($(filter m20lte m30lte a40 a30 gta3xlwifi, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- releasetools.py
+++ releasetools.py
@@ -1,26 +1,16 @@
-#!/bin/env python3
-#
-# Copyright (C) 2021 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import common
-import re
-
 def FullOTA_Assertions(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-bootloader-min\s*=\s*(\S+)', android_info)
-  if m:
-    bootloader_version = m.group(1)
-    cmd = ('assert(universal7904.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires Android 10 or 11 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
-    info.script.AppendExtra(cmd)
+    info.script.AppendExtra('ifelse(is_mounted("/vendor"), unmount("/vendor"));');
+    info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/13500000.dwmmc0/by-name/vendor", "/vendor");');
+    info.script.AppendExtra('ifelse(file_getprop("/vendor/build.prop","ro.vendor.build.version.sdk") != "29",');
+    info.script.AppendExtra('unmount("/vendor");');
+    info.script.AppendExtra('ui_print(" ");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("                    ERROR:                   ");');
+    info.script.AppendExtra('ui_print("           Vendor image on the device        ");');
+    info.script.AppendExtra('ui_print("               is NOT compatible             ");');
+    info.script.AppendExtra('ui_print("           Expected Android 10 vendor        ");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("============NO CHANGE HAS BEEN MADE==========");');
+    info.script.AppendExtra('abort(" "););');
+    info.script.AppendExtra('unmount("/vendor");');
